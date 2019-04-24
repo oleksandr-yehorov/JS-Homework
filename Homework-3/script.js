468424642b15321ebbaa96f3d07737deb808c316
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-04-24 13:03:51 +0300

    removing unnecessary cycles

--- Homework-3/script.js
+++ Homework-3/script.js
@@ -35,39 +35,6 @@ function chooseExpenses() {
 
 chooseExpenses();
 
-// let i = 0;
-
-// do...while
-
-/*do {
-    let a = prompt('Введіть обов\'язкову статтю видатків у цьому місяці:', ''),
-    b = prompt('В яку суму обійдеться?', '');
-    if ((typeof(a) === 'string') && typeof(a) != null && typeof(b) != null
-        && a != '' && b != '' && a.length < 50) {
-            console.log("done");
-            appData.expenses[a] = b;
-    } else {
-        i--;
-    }
-    i++;
-    } while (i<2);*/
-
-// while...
-
-/*while (i<2) {
-    let a = prompt('Введіть обов\'язкову статтю видатків у цьому місяці:', ''),
-    b = prompt('В яку суму обійдеться?', '');
-    if ((typeof(a) === 'string') && typeof(a) != null && typeof(b) != null
-        && a != '' && b != '' && a.length < 50) {
-            console.log("done");
-            appData.expenses[a] = b;
-    } else {
-        i--;
-    }
-    i++;
-};*/
-
-
 appData.moneyPerDay = appData.budget/30;
 
 alert("Щоденний бюджет: " + appData.moneyPerDay);
