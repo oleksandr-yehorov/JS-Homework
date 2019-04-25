5f7bb4b8a8f206eff5141dd596ea7cbd689360f6
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-04-25 13:19:50 +0300

    Add changeImage function

--- Homework-5/js/script.js
+++ Homework-5/js/script.js
@@ -4,6 +4,13 @@ function fifthElement(){
     newLi.innerHTML = 'Пятый пункт';
     newLi.classList.add('menu-item');
     menu[0].appendChild(newLi);
-}
+};
 
 fifthElement();
+
+function changeImage() {
+    let image = document.getElementsByTagName('body');
+    image[0].style.background = 'url(img/apple_true.jpg) center no-repeat';
+};
+
+changeImage();
