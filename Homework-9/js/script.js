01174c850287dc715cdbc9abd451804c2078cce1
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-04-30 17:01:24 +0300

    Add time value

--- Homework-9/js/script.js
+++ Homework-9/js/script.js
@@ -34,4 +34,20 @@ window.addEventListener('DOMContentLoaded', function(){
         }
     });
 
+    // Timer
+
+    let deadLine = '2019-05-25';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+        return {
+            'total': t,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours
+        }
+    };
 })
