b7c41146299f7255545dcb7d0d1ad1c7c89f15c5
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-05-02 17:17:15 +0300

    Homework-12

--- Homework-12/js/script.js
+++ Homework-12/js/script.js
@@ -66,7 +66,7 @@ window.addEventListener('DOMContentLoaded', function(){
             hours.textContent = t.hours;
             };
             if (t.minutes<=9) {
-                miniutes.textContent = `0${t.minutes}`; 
+                minutes.textContent = `0${t.minutes}`; 
             } else {
             minutes.textContent = t.minutes;
             };
@@ -113,4 +113,52 @@ window.addEventListener('DOMContentLoaded', function(){
     
     };
     
+    // Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с Вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', '/server.php');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(key, value){
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+
+    });
 })
--- Homework-12/server.php
+++ Homework-12/server.php
@@ -1,3 +1,5 @@
 <?php 
-
+header('Access-Control-Allow-Origin: null');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: content-type');
 echo $_SERVER[‘REMOTE_ADDR‘];
