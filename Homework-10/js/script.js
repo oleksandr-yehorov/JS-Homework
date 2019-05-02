c449199a22dd90e022797279ca32e85447848083
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-05-02 13:32:27 +0300

    Add modal window

--- Homework-10/js/script.js
+++ Homework-10/js/script.js
@@ -83,4 +83,23 @@ window.addEventListener('DOMContentLoaded', function(){
     };
 
     setClock('timer', deadLine);
+
+    // Modals
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+    
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    })
+
 })
